2077cf5db3ad6bb344faa5d757104e2816eaa621
Minzyx <minzybtw@gmail.com>
2024-05-29 17:03:01 -0300

    feat: breakout

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, Color, Engine, Font, FontUnit, Label, Loader, Sound, Text, vec } from "excalibur"
+import { Actor, CollisionType, Color, ColorBlindnessMode, Engine, Font, FontUnit, Label, Loader, Sound, Text, vec } from "excalibur"
 
 // 1 - Criar uma instancia de Engine, que representa o jogo
 const game = new Engine({
@@ -30,25 +30,39 @@ game.input.pointers.primary.on("move", (event) => {
 // 4 - Criar o Actor bolinha
 const bolinha = new Actor({
 	x: 100,
-	y:300,
+	y: 300,
 	radius: 10,
 	color: Color.Red
 })
 
 bolinha.body.collisionType = CollisionType.Passive
 
+let coresBolinha = [
+	Color.Black, 
+	Color.Chartreuse,
+	Color.Orange, 
+	Color.Orange,
+	Color.Magenta, 
+	Color.Cyan,
+	Color.Red,
+	Color.Rose,
+	Color.Yellow,
+	Color.White,
+]
+
+let numeroCores = coresBolinha.length
 
 // 5 - Criar movimentação da bolinha
-const velocidadeBolinha = vec(350, 350)
+const velocidadeBolinha = vec(1300, 1300)
 
-setTimeout(()=> {
+setTimeout(() => {
 	bolinha.vel = velocidadeBolinha
 }, 1000)
 
 // 6 - Fazer bolinha bater na parede
 bolinha.on("postupdate", () => {
 	// Se a bolinha colidir com o lado esquerdo	
-	if (bolinha.pos.x < bolinha.width / 2){
+	if (bolinha.pos.x < bolinha.width / 2) {
 		bolinha.vel.x = velocidadeBolinha.x
 	}
 	// Se a bolinha colidir com o lado direito
@@ -56,14 +70,14 @@ bolinha.on("postupdate", () => {
 		bolinha.vel.x = -velocidadeBolinha.x
 	}
 	// Se a bolinha colidir com o lado superior
-	if (bolinha.pos.y < bolinha.height / 2){
+	if (bolinha.pos.y < bolinha.height / 2) {
 		bolinha.vel.y = velocidadeBolinha.y
 	}
 	// Se a bolinha colidir com o lado inferior
 	// if (bolinha.pos.y + bolinha.height / 2 > game.drawHeight){
 	// 	bolinha.vel.y = -velocidadeBolinha.y
 	// }
-	
+
 })
 
 // Insere bolinha no game
@@ -87,15 +101,15 @@ const larguraBloco = (game.drawWidth / colunas) - padding - (padding / colunas)
 const alturaBloco = 30
 // const larguraBloco = 136
 
-const listaBlocos: Actor [] = []
+const listaBlocos: Actor[] = []
 
 // Renderização dos bloquinhos
 
 // Renderiza 3 linhas
-for(let j = 0; j < linhas; j++) {
-	
+for (let j = 0; j < linhas; j++) {
+
 	// Renderiza 5 bloquinhos
-	for(let i = 0; i < colunas; i++ ){
+	for (let i = 0; i < colunas; i++) {
 		listaBlocos.push(
 			new Actor({
 				// indica o deslocamento de um bloco pro outro
@@ -113,7 +127,7 @@ for(let j = 0; j < linhas; j++) {
 
 
 
-listaBlocos.forEach( bloco => {
+listaBlocos.forEach(bloco => {
 	bloco.body.collisionType = CollisionType.Active
 
 	game.add(bloco)
@@ -131,17 +145,19 @@ const textoPontos = new Label({
 		strokeColor: Color.Black,
 		unit: FontUnit.Px
 	}),
-	pos: vec(600,500)
+	pos: vec(600, 500)
 })
 
 game.add(textoPontos)
+
+
 
 // const textoPontos = new Text({
 // 	text: "Hello World",
 // 	font: new Font({size:20})
 // })
 
-const objetoTexto = new Actor ({
+const objetoTexto = new Actor({
 	x: game.drawWidth - 80,
 	y: game.drawHeight - 15
 })
@@ -152,60 +168,75 @@ game.add(objetoTexto)
 
 let colidindo: boolean = false
 
+// Adicionando sonzinhoss
 const sound = new Sound("./sound/sonzinho.mp3");
-const loader = new Loader([sound]);
+const gameOverSound = new Sound ("./sound/perdeu.mp3");
+const loader = new Loader([sound, gameOverSound]);
 
 
-bolinha.on("collisionstart",(event) => {
+bolinha.on("collisionstart", (event) => {
 
-	if (pontos < blocos) {
-		
-		// Verificar se a bolinha colidiu com algum bloco destrutivel
-		console.log()
-		
-		// Se o elemento colidido for um bloco da lista de blocos (destrutivel)
-		
-		if (listaBlocos.includes(event.other)) {
-			// Destruir o bloco colidido
-			event.other.kill()
-			
-			pontos++
-			
-			textoPontos.text = pontos.toString()
-			sound.play(0.5)
-			
-			
-		} 
-		// Rebater a bolinha - Inverter as direções
-		// "minimum translation vector" is a vector `normalize()`
-		let interseccao = event.contact.mtv.normalize()
-		
-		// Se não está colidindo
-		if (!colidindo) {
-			colidindo = true
-			// interseccao.x e interseccao.y
-			// O maior representa o eixo onde houve o contato
-			if ( Math.abs(interseccao.x)> Math.abs(interseccao.y)) {
-				bolinha.vel.x = bolinha.vel.x * -1
-			}else{
-				bolinha.vel.y = bolinha.vel.y * -1
-			}
-		}
-	} else{
-		alert("Ganhooooou!!")
-		window.location.reload()
+
+
+	// Verificar se a bolinha colidiu com algum bloco destrutivel
+	console.log()
+
+	// Se o elemento colidido for um bloco da lista de blocos (destrutivel)
+
+	if (listaBlocos.includes(event.other)) {
+		// Destruir o bloco colidido
+		event.other.kill()
+
+		pontos++
+
+		// Mudar a cor da bolinha
+		bolinha.color = coresBolinha[Math.trunc (Math.random() * numeroCores) ]
+		// Math.random -> 0 - 1 * numeroCores -> 10
+		// 0.5 * 10 = 5
+		// 0.3 * 10 = 3
+		// 0.873 * 10  = 8.73
+
+		// Math.trunc() -> retorna somente a porção inteira de um numero
+		textoPontos.text = pontos.toString()
+		sound.play(0.5)
+
+
 	}
-	} )
+	// Rebater a bolinha - Inverter as direções
+	// "minimum translation vector" is a vector `normalize()`
+	let interseccao = event.contact.mtv.normalize()
 
-	
+	// Se não está colidindo
+	if (!colidindo) {
+		colidindo = true
+		// interseccao.x e interseccao.y
+		// O maior representa o eixo onde houve o contato
+		if (Math.abs(interseccao.x) > Math.abs(interseccao.y)) {
+			bolinha.vel.x = bolinha.vel.x * -1
+		} else {
+			bolinha.vel.y = bolinha.vel.y * -1
+		}
 
-bolinha.on("collisionend", () => {
-	colidindo=false
-})
+	}})
 
-bolinha.on("exitviewport", () => {
-	alert("Morreu")
-	window.location.reload()
-})
-// Inicia o game
-game.start(loader)
+
+
+	bolinha.on("collisionend", () => {
+		colidindo = false
+
+		if (pontos == blocos) {
+			alert("Ganhoou :) ")
+			window.location.reload()
+		}
+	})
+
+	bolinha.on("exitviewport", () => {
+		gameOverSound.play(1)
+		.then(() =>{
+			alert("Perdeuu :( ")
+
+			window.location.reload()
+		})
+	})
+	// Inicia o game
+	await game.start(loader)
